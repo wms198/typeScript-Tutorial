5ca37098f4a0e9409131904e3037dffaaf107945
wms198 <wang.minshi@icloud.com>
2024-08-15 09:55:37 +0200

    Tutorial 5

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,38 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-//character =30;
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(7.5));
+// Tutorial 5
+// Explicit types
+var character;
+var age;
+var isLoggenin;
+//arrays
+var ninjas = [];
+ninjas.push("yoshi");
+//union types
+var mixed = [];
+mixed.push(20);
+mixed.push("hello");
+console.log(mixed);
+var uid;
+uid = "123";
+uid = 123;
+//object
+var ninhaOne;
+ninhaOne = { name: "yoshi", age: 30 };
+var ninhaTwo;
+ninhaTwo = { name: "mario", age: 20, beltColor: "black" };
 /*
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+//character =30;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5));
+
+
 console.log(character);
 
 const inputs = document.querySelectorAll('input');
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,39 @@
+// Tutorial 5
+// Explicit types
+let character: string;
+let age: number;
+let isLoggenin: boolean;
+ 
+//arrays
+let ninjas: string[] = [];
+ninjas.push("yoshi");
+
+//union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push(20);
+mixed.push("hello");
+console.log(mixed);
+
+let uid: string|number;
+uid = "123";
+uid = 123;
+
+//object
+let ninhaOne: object;
+ninhaOne = {name: "yoshi", age: 30};
+
+let ninhaTwo: {
+    name: string;
+    age: number;
+    beltColor: string;
+};
+
+ninhaTwo = {name: "mario", age: 20, beltColor: "black"};
+
+
+
+
+/*
 let character = 'mario';
 let age = 30;
 let isBlackBelt = false;
@@ -10,7 +46,7 @@ const circ = (diameter: number) => {
 
 console.log(circ(7.5));
 
-/*
+
 console.log(character);
 
 const inputs = document.querySelectorAll('input');
