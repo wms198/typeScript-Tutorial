2e7487d09105465536d290ff2eba48db5cbb2c33
wms198 <wang.minshi@icloud.com>
2024-08-15 09:06:38 +0200

    follow the youtuber Net Ninja's typescript tutorial untill 4th

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+//character =30;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
+/*
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input =>{
+    console.log(input);
+})
+ */
---
+++ sandbox.ts
@@ -0,0 +1,23 @@
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+//character =30;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+} 
+
+console.log(circ(7.5));
+
+/*
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input =>{
+    console.log(input);
+})
+ */
